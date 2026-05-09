046a2ecafb62bb1cdffbb2a497392a1ac9e46d5e
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-09 21:58:28 +0600

    play and random alpha generate

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,8 @@
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+}
 function play() {
-
+    hideElementById('home-screen');
+    ShowElementById('play-ground');
+    continueGame();
 }
---
+++ scripts/utility.js
@@ -0,0 +1,16 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+function ShowElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+function getARandomAlphabet() {
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz"
+    const alphabets = alphabetString.split(' ');
+    const randomNumber = Math.random() * 25;
+    const indexx = Math.round(randomNumber);
+    const alphabet = alphabets[indexx]
+    return alphabet;
+}
