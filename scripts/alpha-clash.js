24e45c8b6c308fb53819c20a5ea66b4a3178bf55
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-10 14:15:50 +0600

    score and life updates

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -4,13 +4,19 @@ function handleKeyboardKeyUpEvent(event) {
     const currentAlphabetElement = document.getElementById('current-alphabet')
     const expectedAlphabet = currentAlphabetElement.innerText.toLowerCase()
     if (playerPressed === expectedAlphabet) {
-        console.log('You get a point')
-        removeBackgroundColorById(expectedAlphabet)
-        continueGame()
+        console.log('You get a point');
+        const currentScore = getTextElementValueById('current-score')
+        const newScore = currentScore + 1;
+        setTextElementValueById('current-score', newScore)
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
 
     }
     else {
         console.log('You dont get a point')
+        const currentLife = getTextElementValueById('current-life')
+        const newLife = currentLife - 1;
+        setTextElementValueById('current-life', newLife)
     }
 }
 document.addEventListener('keyup', handleKeyboardKeyUpEvent)
--- scripts/utility.js
+++ scripts/utility.js
@@ -14,6 +14,17 @@ function removeBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const value = parseInt(element.innerText);
+    return value;
+
+}
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
     const alphabets = alphabetString.split('');
