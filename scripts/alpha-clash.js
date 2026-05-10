c65571ac0ef3de44607b19c6910d53d3e4b3aaab
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-10 12:15:02 +0600

    keyboard press js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,19 @@
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key
+
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    const expectedAlphabet = currentAlphabetElement.innerText.toLowerCase()
+    if (playerPressed === expectedAlphabet) {
+        console.log('You get a point')
+        removeBackgroundColorById(expectedAlphabet)
+        continueGame()
+
+    }
+    else {
+        console.log('You dont get a point')
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
 function continueGame() {
     const alphabet = getARandomAlphabet();
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,13 +10,15 @@ function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
     const alphabets = alphabetString.split('');
-
     const randomNumber = Math.random() * 25;
     const indexx = Math.round(randomNumber);
     const alphabet = alphabets[indexx];
-    console.log(alphabet, indexx)
     return alphabet;
 }
